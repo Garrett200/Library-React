d500e1e320253aec635b41c472bd03b15ba3f807
Garrett <garrettjrh@live.com>
2023-07-06 01:16:35 -0600

    Had an issue with upgrading to react v6, the app now runs.

--- App.js
+++ App.js
@@ -1,36 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import BookInfo from "./pages/BookInfo";
 import { books } from "./data";
 import Nav from "./components/Nav";
-import Footer from "./components/Footer";
 import Cart from "./pages/Cart";
 import { counter } from "@fortawesome/fontawesome-svg-core";
 
 function App() {
   const [cart, setCart] = useState([]);
 
-  function addItemToCart(book) {
+  function addToCart(book) {
     const dupeItem = cart.find((item) => item.id === book.id);
     setCart((oldCart) =>
       dupeItem
         ? [
-            ...oldCart.map((item) => {
-              return item.id === dupeItem.id
-                ? {
-                    ...item,
-                    quantity: item.quantity + 1,
-                  }
-                : item;
-            }),
-          ]
+          ...oldCart.map((item) => {
+            return item.id === dupeItem.id
+              ? {
+                ...item,
+                quantity: item.quantity + 1,
+              }
+              : item;
+          }),
+        ]
         : [...oldCart, { ...book, quantity: 1 }]
     );
   }
 
-  function updateCart(item, newQuantity) {
+  function changeQuantity(item, newQuantity) {
     setCart((oldCart) =>
       oldCart.map((oldItem) => {
         if (oldItem.id === item.id) {
@@ -50,15 +50,6 @@ function App() {
   }
 
   function numberOfItems() {
-    let counter = 0;
-    cart.forEach((item) => {
-      counter += +item.quantity;
-    });
-    return counter;
-  }
-
-  function numberOfItems() {
-    let counter = 0;
     cart.forEach((item) => {
       counter += +item.quantity;
     });
@@ -81,26 +72,12 @@ function App() {
     <Router>
       <div className="App">
         <Nav numberOfItems={numberOfItems()} />
-        <Route path="/" exact render={() => <Home books={books} />} />
-        <Route path="/books" exact render={() => <Books books={books} />} />
-        <Route
-          path="/books/:id"
-          render={() => (
-            <BookInfo books={books} addItemToCart={addItemToCart} />
-          )}
-        />
-        <Route
-          path="/cart"
-          render={() => (
-            <Cart
-              cart={cart}
-              updateCart={updateCart}
-              removeItem={removeItem}
-              totals={calcPrices()}
-            />
-          )}
-        />
-        <Footer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route path="/book/:id" element={<BookInfo books={books} addToCart={addToCart} cart={cart} />} />
+          <Route path="/cart" element={<Cart cart={cart} book={books} changeQuantity={changeQuantity} />} />
+        </Routes>
       </div>
     </Router>
   );
